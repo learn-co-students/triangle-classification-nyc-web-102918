d34ad18b1a5274877e3626a048cdd9d432cbcb90
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-08 12:52:43 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,9 @@ class Triangle
 
   def kind
     if (side1+side2 <= side3 || side1+side3 <= side2 || side2+side3 <= side1 || side1 <= 0 || side2 <= 0 || side3 <= 0)
-      # binding.pry
       begin
         raise TriangleError
-      #rescue TriangleError => error
-      #  puts error.message
+
       end
     elsif side1==side2 && side1==side3
        :equilateral
